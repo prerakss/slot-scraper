0513af8bbc8c5688ab676cca2bc36c05a07a07af
prerakss <89816387+prerakss@users.noreply.github.com>
2024-10-18 19:03:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import os
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 # Function to fetch the JSON data
 def get_json_data():
@@ -29,7 +31,6 @@ def get_json_data():
         return None
     return response.json()
 
-# Function to format the table data
 def format_table(data):
     formatted_data = ""
     
@@ -37,15 +38,24 @@ def format_table(data):
     for visa_type, records in data.items():
         if records['H-1B (Dropbox)']:
             for record in records['H-1B (Dropbox)']:
+                # Convert the GMT time to Eastern Time
+                gmt_time = datetime.strptime(record['createdon'], '%Y-%m-%d %H:%M:%S')
+                gmt_time = gmt_time.replace(tzinfo=ZoneInfo('GMT'))
+                eastern_time = gmt_time.astimezone(ZoneInfo("America/New_York"))
+                
+                # Format the Eastern time as a string
+                eastern_time_str = eastern_time.strftime('%Y-%m-%d %H:%M:%S %Z')
+                
                 formatted_data += (
                     f"Location: {record['visa_location']}, "
                     f"Earliest Date: {record['earliest_date']}, "
                     f"Total Dates: {record['no_of_dates']}, "
-                    f"Last Seen: {record['createdon']}, "
+                    f"Last Seen: {eastern_time_str}, "
                     f"Appointments: {record['no_of_apnts']}\n"
                 )
             print(formatted_data)
         return formatted_data
+
 
 def send_push_notification(message):
     user_key = os.getenv('PUSHOVER_USER_KEY')
@@ -80,8 +90,8 @@ def check_for_updates():
         old_table = None
 
     print(old_table)
-    # Compare the new data with the old data
-    if old_table != new_table:
+    # Compare the new data with the old data and check if appointments > 0
+    if old_table != new_table and any(int(line.split("Appointments: ")[1]) > 0 for line in new_table.split('\n') if line):
         send_push_notification(new_table)
 
         # Save the new data to the file
